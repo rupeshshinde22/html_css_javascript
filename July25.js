80303a2ac24ac1c11257988b999c8af09c0989d1
rupeshshinde22 <rupesh.shinde@springernature.com>
2022-07-25 15:06:43 +0530

    question 1.JavaScript Program to Find the Factorial of a Number completed

--- July25.js
+++ July25.js
@@ -1,3 +1,27 @@
-Javascript task :-
+//Javascript task :-
+//
+//1.JavaScript Program to Find the Factorial of a Number
+<html>
+<head>
+    <title>Factorial</title>
+</head>
+<body>
+<script>
+function factorial(n){
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }
+}
+let n = 4;
+answer = factorial(n)
+console.log(answer);
 
-1.JavaScript Program to Find the Factorial of a Number
+</script>
+</body>
+</html>
